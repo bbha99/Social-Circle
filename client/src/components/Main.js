5a0393feb7c49b3f257c85d1b877ee21d35b046d
Samson <samsonkfpoon@gmail.com>
2023-03-24 09:52:32 -0700

    add new route for users/id

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -6,6 +6,7 @@ import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 import Register from '../pages/Register';
 import AdminDashboard from '../pages/AdminDashboard';
+import User from '../pages/User';
 
 const Main = () => {
   return (
@@ -16,6 +17,7 @@ const Main = () => {
       <Route path='/register' element={<Register />}></Route>
       <Route path='/login' element={<Login />}></Route>
       <Route path='/admin/dashboard' element={<AdminDashboard />}></Route>
+      <Route path='/users/:userId' element={<User />}></Route>
     </Routes>
   );
 };
