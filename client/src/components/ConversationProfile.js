6d222a255c981df282e43ca78bff663e5ef152dd
bbha99 <branha99@gmail.com>
2023-03-25 18:04:07 -0700

    Add view chatted profiles

---
+++ client/src/components/ConversationProfile.js
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+import { Avatar, Box, Button, Card, CardActionArea, CardContent, CardMedia, List, ListItemButton, ListItemText, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState, useContext } from 'react';
+import { authContext } from '../providers/AuthProvider';
+import { Link } from 'react-router-dom';
+
+const Div = styled('div')({
+  margin: (theme) => theme.spacing(1),
+});
+
+const ConversationProfile = (props) => {
+  const [receivers, setReceivers] = useState([]);
+
+  const { user } = useContext(authContext);
+
+  useEffect(() => {
+    axios.post(`http://localhost:3001/conversations`, {
+      id: user.id
+    }, { withCredentials: true })
+      .then((response) => {
+        setReceivers(response.data.users.slice(0, 5));
+      });
+  }, []);
+
+  const suggestedChatPeople = receivers.map((person) => {
+    return (
+      <Box key={person.id}>
+        <Link to={`/users/${person.id}`}>
+          <Avatar sx={{ width: 30, height: 30 }} src={person.image} /> {person.username}</Link>
+      </Box>
+    );
+  });
+
+  return (
+    <Card sx={{marginTop: "70px"}}>
+      <CardContent>
+        <Typography variant="h5">
+          View Conversation Profiles
+        </Typography>
+        {suggestedChatPeople}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ConversationProfile;
