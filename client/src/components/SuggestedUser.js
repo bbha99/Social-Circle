ae1d2f793674af7446f1171e12a8c6bc02f2542a
bbha99 <branha99@gmail.com>
2023-03-25 18:05:15 -0700

    Change view profile to new users only

--- client/src/components/SuggestedUser.js
+++ client/src/components/SuggestedUser.js
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 import { Avatar, Box, Button, Card, CardActionArea, CardContent, CardMedia, List, ListItemButton, ListItemText, TextField, Typography } from '@mui/material';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { authContext } from '../providers/AuthProvider';
 import { Link } from 'react-router-dom';
+import SearchBar from './Searchbar';
 
 const Div = styled('div')({
   margin: (theme) => theme.spacing(1),
@@ -10,10 +12,20 @@ const Div = styled('div')({
 
 const SuggestedUser = (props) => {
   const [searchResults, setSearchResults] = useState([]);
+  const [receivers, setReceivers] = useState([]);
 
+  const { user } = useContext(authContext);
 
   useEffect(() => {
-    axios
+    if (user) {
+      axios.post(`http://localhost:3001/meet_people`, {
+        id: user.id
+      }, { withCredentials: true })
+        .then((response) => {
+          setReceivers(response.data.users);
+        });
+    } else {
+      axios
       .get(`http://localhost:3001/users`)
       .then((response) => {
         setSearchResults(response.data.users.slice(0, 5));
@@ -21,9 +33,19 @@ const SuggestedUser = (props) => {
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+    }
 
-  const suggestedPeople = searchResults.map((person) => {
+    axios
+      .get(`http://localhost:3001/users`)
+      .then((response) => {
+        setReceivers(response.data.users.slice(0, 5));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  
+  const suggestedPeople = receivers.slice(0, 5).map((person) => {
     return (
       <Box key={person.id}>
         <Link to={`/users/${person.id}`}>
@@ -33,11 +55,12 @@ const SuggestedUser = (props) => {
   });
 
   return (
-    <Card sx={{ marginTop: "20px" }}>
+    <Card sx={{ marginTop: "40px" }}>
       <CardContent>
-        <Typography variant="h3">
-          Find Discussers
+        <Typography variant="h5">
+          {user ? "Meet New People" : "Meet People"}
         </Typography>
+        <SearchBar />
         {suggestedPeople}
       </CardContent>
     </Card>
