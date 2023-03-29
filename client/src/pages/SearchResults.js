79ee018c77474014c15031d9e2e95344afe6a51c
Samson Poon <samsonkfpoon@gmail.com>
2023-03-29 06:50:16 +0000

    add styling to user profile, search results page

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { authContext } from "../providers/AuthProvider";
@@ -26,7 +26,6 @@ const SearchResults = () => {
       })
       .then((response) => {
         const postData = response.data.postDetails;
-
         const filteredPosts = postData.filter((post) => {
           const title = post.postsDetails.title.toLowerCase();
           const description = post.postsDetails.description.toLowerCase();
@@ -43,18 +42,20 @@ const SearchResults = () => {
 
   const theme = createTheme({
     typography: {
-      fontSize: 18
+      fontSize: 18,
+      
     }
   });
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ backgroundColor: "#EAF6FF", minHeight: "100vh" }}>
         <Navbar />
         <Box sx={{ maxWidth: "800px", mx: "auto", p: 2 }}>
-          {searchResults.map((result) => {
-            {
+          <Typography variant="h4">Results for {searchInput}</Typography>
+          <Box sx={{ my: 2 }}>
+            {searchResults.map((result) => {
               if (result.postsDetails.deleted !== true) {
                 return (
                   <Box
@@ -62,21 +63,26 @@ const SearchResults = () => {
                     sx={{
                       my: 2,
                       p: 2,
-                      border: "1px solid #ccc",
-                      borderRadius: "4px",
+                      borderRadius: "8px",
+                      border: "2px solid #E0E0E0",
+                      bgcolor: "#FFFFFF"
                     }}
                   >
-                    <Typography variant="h4">{result.postsDetails.title} </Typography>
-                    <Typography variant="h6">Posted By {result.postsDetails.user.username} {moment(result.postsDetails.user.created_at).fromNow()}</Typography>
+                    <Typography variant="h5">{result.postsDetails.title} </Typography>
+                    <Typography variant="h6">
+                      Posted By {result.postsDetails.user.username}{" "}
+                      {moment(result.postsDetails.user.created_at).fromNow()}
+                    </Typography>
                     <Typography>{result.postsDetails.description}</Typography>
+                    <Typography>{result.postsDetails.image}</Typography>
                   </Box>
                 );
               }
-            }
-          })}
+            })}
+          </Box>
         </Box>
-      </ThemeProvider>
-    </>
+      </Box>
+    </ThemeProvider>
   );
 };
 
